929c2ee49e3d1d945e37941bf505240f6227aca6
lpcwww <lpc.www.com@gmail.com>
2017-11-01 22:43:56 +0900

    mip

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,16 +3,17 @@ var router = express.Router();
 var emotion = require('../app/emotion');
 
 /* GET home page. */
-var imgUrl = 'https://cdn.liginc.co.jp/wp-content/uploads/2015/12/18ffe47ae091488567f5b7bacfbe9db710.jpg';
+var imgUrl = 'https://cdn.liginc.co.jp/wp-content/uploads/2015/11/b0bc307347cc4ced1f66d0f567f672531-210x210.jpg';
 
 emotion.getEmotion(imgUrl)
     .then(function (value) {
         router.get('/', function(req, res, next) {
             res.render('index', {
-                title: 'Express',
+                title: 'はつしのきもち',
                 emotionRespons: value
             });
         });
     });
+
 
 module.exports = router;
